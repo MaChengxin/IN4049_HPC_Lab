f8af2de1539616535e3d3f121a05b5ba5aaaf872
Chengxin <machengxinchina@gmail.com>
2017-01-14 20:12:54 +0100

    Update experiment results

--- FEMPois/experiment/3.5/dataAnalysis.m
+++ FEMPois/experiment/3.5/dataAnalysis.m
@@ -1,8 +1,8 @@
 %% Data analysis
 clear;
 
-load timer_28.dat;
-avg = mean(timer_28);
+load timer.dat;
+avg = mean(timer);
 percentage_computation = avg(3)/avg(2);
 percentage_exchange_borders = avg(4)/avg(2);
 percentage_global_communication = avg(5)/avg(2);
