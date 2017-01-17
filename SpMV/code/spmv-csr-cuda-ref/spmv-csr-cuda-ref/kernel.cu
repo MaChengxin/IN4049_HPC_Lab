8ec91c2f39336ff28b6160d4b7b0066f25dbfc9e
Chengxin <machengxinchina@gmail.com>
2017-01-17 12:51:22 +0100

    Refactor code

--- SpMV/code/spmv-csr-cuda-ref/spmv-csr-cuda-ref/kernel.cu
+++ SpMV/code/spmv-csr-cuda-ref/spmv-csr-cuda-ref/kernel.cu
@@ -1,95 +1,83 @@
-// parallel SPMV CSR
-// compute y = y + Ax
-// Assume all the input element of vector y[i] and x[i] are "i", and length(y) = length(x) = col[A]
-//
-// G. Fu, Dec 31, 2015
-//
+/*
+Parallel SPMV CSR
+Compute y = y + Ax
+G. Fu, Dec 31, 2015
+Modified by C. Ma
+*/
 
 #include <stdio.h>
 #include <cuda.h>
 #include <device_launch_parameters.h>
 #include <cuda_runtime.h>
 
-// used in csrmul_kernel
-__device__ float multiply_row(int rowsize,
-	int *Aj,      // column indices for row
-	float *Av,    // non-zero entries for row
-	float *x)     // the RHS vector
+__device__ float multiply_row(int rows_size, int *idx, float *val, float *x)
 {
-	float sum = 0;
-	for (int column = 0; column < rowsize; ++column)
-		sum += Av[column] * x[Aj[column]];
+	float sum = 0.0;
+	for (int i = 0; i < rows_size; ++i)
+		sum += val[i] * x[idx[i]];
 	return sum;
 }
 
-// used in main
-__global__ void csrmul_kernel(int *Ap, int *Aj, float *Av, int num_rows_A, float *x, float *y)
+__global__ void csrmul_kernel(int *A_ptr, int *A_idx, float *A_val, int num_rows_A, float *x, float *y)
 {
-	int row = blockIdx.x * blockDim.x + threadIdx.x;  //parallel by the row number
-	if (row < num_rows_A)
-	{
-		int row_begin = Ap[row];
-		int row_end = Ap[row + 1];
-		y[row] = y[row] + multiply_row(row_end - row_begin, Aj + row_begin, Av + row_begin, x);
-	}
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if (i < num_rows_A)
+		y[i] = y[i] + multiply_row(A_ptr[i + 1] - A_ptr[i], A_idx + A_ptr[i], A_val + A_ptr[i], x);
 }
 
 int main(void)
 {
-	const int num_Ap = 5;
-	const int num_Av = 9;
-	const int num_rows_A = 4;
-	const int num_rows_x = 4;
-	const int num_rows_y = 4;
+	int A_ptr[] = { 0, 2, 4, 7, 9 };
+	int A_idx[] = { 0, 1, 1, 2, 0, 2, 3, 1, 3 };
+	float A_val[] = { 1.0, 7.0, 2.0, 8.0, 5.0, 3.0, 9.0 , 6.0, 4.0 };
+	float x[] = { 1.0, 2.0, 3.0, 4.0 };
+	float y[] = { 1.0, 2.0, 3.0, 4.0 };
 
-	int Ap_host[] = { 0, 2, 4, 7, 9 };
-	int Aj_host[] = { 0, 1, 1, 2, 0, 2, 3, 1, 3 };
-	float Av_host[] = { 1.0, 7.0, 2.0, 8.0, 5.0, 3.0, 9.0 , 6.0, 4.0 };
-	float x_host[] = { 1.0, 2.0, 3.0, 4.0 };
-	float y_host[] = { 1.0, 2.0, 3.0, 4.0 };
+	int num_rows_A = 4;
+	int num_rows_y = sizeof(y) / sizeof(y[0]);
 
-	int *Ap;
-	int *Aj;
-	float *Av;
-	float *x;
-	float *y;
+	for (int i = 0; i < num_rows_y ; i++)
+		printf("input y(%i) = %f\n", i, y[i]);
 
-	int size_Ap = num_Ap * sizeof(int);
-	int size_Aj = num_Av * sizeof(int);
-	float size_Av = num_Av * sizeof(float);
-	float size_x = num_rows_x * sizeof(float);
-	float size_y = num_rows_y * sizeof(float);
+	int *A_ptr_dev;
+	int *A_idx_dev;
+	float *A_val_dev;
+	float *x_dev;
+	float *y_dev;
 
-	for (int i = 0; i <= num_rows_y - 1; i++)
-		printf("input y(%i) = %f\n", i, y_host[i]);
+	int size_A_ptr = sizeof(A_ptr);
+	int size_A_idx = sizeof(A_idx);
+	float size_A_val = sizeof(A_val);
+	float size_x = sizeof(x);
+	float size_y = sizeof(y);
 
-	cudaMalloc((void**)&Ap, size_Ap);
-	cudaMalloc((void**)&Aj, size_Aj);
-	cudaMalloc((void**)&Av, size_Av);
-	cudaMalloc((void**)&x, size_x);
-	cudaMalloc((void**)&y, size_y);
+	cudaMalloc((void**)&A_ptr_dev, size_A_ptr);
+	cudaMalloc((void**)&A_idx_dev, size_A_idx);
+	cudaMalloc((void**)&A_val_dev, size_A_val);
+	cudaMalloc((void**)&x_dev, size_x);
+	cudaMalloc((void**)&y_dev, size_y);
 
-	cudaMemcpy(Ap, Ap_host, size_Ap, cudaMemcpyHostToDevice);
-	cudaMemcpy(Aj, Aj_host, size_Aj, cudaMemcpyHostToDevice);
-	cudaMemcpy(Av, Av_host, size_Av, cudaMemcpyHostToDevice);
-	cudaMemcpy(x, x_host, size_x, cudaMemcpyHostToDevice);
-	cudaMemcpy(y, y_host, size_y, cudaMemcpyHostToDevice);
+	cudaMemcpy(A_ptr_dev, A_ptr, size_A_ptr, cudaMemcpyHostToDevice);
+	cudaMemcpy(A_idx_dev, A_idx, size_A_idx, cudaMemcpyHostToDevice);
+	cudaMemcpy(A_val_dev, A_val, size_A_val, cudaMemcpyHostToDevice);
+	cudaMemcpy(x_dev, x, size_x, cudaMemcpyHostToDevice);
+	cudaMemcpy(y_dev, y, size_y, cudaMemcpyHostToDevice);
 
-	unsigned int blocksize = 128;
-	unsigned int nblocks = (num_rows_A + blocksize - 1) / blocksize;
+	unsigned int block_size = 128;
+	unsigned int num_blocks = (num_rows_A + block_size - 1) / block_size;
 
-	csrmul_kernel << < nblocks, blocksize >> > (Ap, Aj, Av, num_rows_A, x, y);
+	csrmul_kernel << < num_blocks, block_size >> > (A_ptr_dev, A_idx_dev, A_val_dev, num_rows_A, x_dev, y_dev);
 
-	cudaMemcpy(y_host, y, size_y, cudaMemcpyDeviceToHost);
+	cudaMemcpy(y, y_dev, size_y, cudaMemcpyDeviceToHost);
 
-	cudaFree(y);
-	cudaFree(x);
-	cudaFree(Av);
-	cudaFree(Aj);
-	cudaFree(Ap);
+	cudaFree(A_ptr_dev);
+	cudaFree(A_idx_dev);
+	cudaFree(A_val_dev);
+	cudaFree(x_dev);
+	cudaFree(y_dev);
 
-	for (int i = 0; i <= num_rows_y - 1; i++)
-		printf("output y(%i) = %f\n", i, y_host[i]);
+	for (int i = 0; i < num_rows_y ; i++)
+		printf("output y(%i) = %f\n", i, y[i]);
 
 	return EXIT_SUCCESS;
 }
